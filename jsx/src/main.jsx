ecf54b24664f0a0fc58307874f1271eda44e9d19
Sachin7717 <sachindhimann71@gmail.com>
2024-05-31 11:48:16 +0530

    making objects and rendering it on page

--- jsx/src/main.jsx
+++ jsx/src/main.jsx
@@ -1,11 +1,21 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 const google= (
   <a href="https://google.com" target='_blank'>visit google</a>
 )
 
+const youtube = React.createElement(
+  'a',
+  { href: 'https://www.youtube.com/', target: '_blank' },
+  'visit Youtube'
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  google
+  <>
+  {google}
+  <br />
+  {youtube}
+  </>
  
 )
