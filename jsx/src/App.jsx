ce5212347f3c7cc92d489e07f0357395bd4962cd
Sachin7717 <sachindhimann71@gmail.com>
2024-05-31 11:33:56 +0530

    first react app and understanding how create a object and render it on webpage

---
+++ jsx/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hii i am sachin</h1>
+      <p>this iis para</p>
+    </>
+  )
+}
+
+export default App
---
+++ jsx/src/main.jsx
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom/client'
+
+const google= (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  google
+ 
+)
