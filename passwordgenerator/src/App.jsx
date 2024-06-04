4cb43c73d44113e112902ee143316a6bf47c8f06
Sachin7717 <sachindhimann71@gmail.com>
2024-06-04 21:19:50 +0530

    usestate and use callback is initialized and callback function is used

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
+
 import './App.css'
 
 function App() {
+ const [length, setlenght] = useState(8);
+ const [numberAllowed , setnumberAllowed] = useState(false)
+ const [charAllowed , setcharAllowed] = useState(false)
+ const [password , setpassword] = useState("")
 
+ const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*"
+
+    for (let i = 0; i < array.length; i++) {
+      let char = Math.floor(Math.random() * str.length +1)
+      
+    }
+
+    let
+ }, [length,numberAllowed,charAllowed,setpassword])
 
   return (
     <>
      
-        <h1>password generator</h1>
+        <h1 className='text-5xl text-center text-white'>password generator</h1>
     </>
   )
 }
