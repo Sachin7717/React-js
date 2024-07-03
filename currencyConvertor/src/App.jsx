324e485754c9d00492c027f09a13b161ec025ca7
Sachin7717 <sachindhimann71@gmail.com>
2024-07-03 15:39:15 +0530

    making a cureencyy convertor

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-500'>my name is sacchin</h1>
+     
+    </>
+  )
+}
+
+export default App
